e18aa9e8df97297f026a3243717a46925fb0b367
qwewasd <hh222ti@student.lnu.se>
2019-02-14 19:00:12 +0100

    Many changes again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ class Main{
 	public static void main(String[] args){
 		System.out.println("Hangman");
 		for(int i = 0; i < 100; i++){
-			System.out.println(i);
+			System.out.println(i+10);
 		}
 	}
 	
